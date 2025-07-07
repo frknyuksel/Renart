4f84a1eb980e421c63f279b7c10773fdd342ac8d
Furkan <yukselfrkn14@gmail.com>
2025-07-08 02:23:44 +0300

    Font düzenlemeleri

--- Renart/frontend/src/pages/_document.js
+++ Renart/frontend/src/pages/_document.js
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Font Preload  Mobilde render ederken fontların değişime uğramamaması için  */}
+        <link
+          rel="preload"
+          href="/fonts/montserat/Montserrat-Regular.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/montserat/Montserrat-Medium.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/avenir/Avenir-Book.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
